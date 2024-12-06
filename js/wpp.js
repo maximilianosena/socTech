2db118e9b175ba102ea4cbdc9106c37cf835ca6b
Maximiliano Sena <130717933+maximilianosena@users.noreply.github.com>
2024-12-06 14:52:04 -0300

    wpp

--- js/wpp.js
+++ js/wpp.js
@@ -2,13 +2,11 @@ document.getElementById('messageForm').addEventListener('submit', (event) => {
     event.preventDefault();
 
     const name = document.getElementById('name').value.trim();
-    const userPhone = document.getElementById('phone').value.trim();
     const message = document.getElementById('message').value.trim();
     const companyPhone = '59895827701'; 
 
     // Construye el mensaje para WhatsApp
-    const whatsappMessage = `Hola, soy ${encodeURIComponent(name)}. 
-Mi número de teléfono es ${encodeURIComponent(userPhone)}. 
+    const whatsappMessage = `Hola, soy ${encodeURIComponent(name)}.  
 ${encodeURIComponent(message)}`;
 
     
