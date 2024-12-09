b0d23dcf010ff407aef1c42fa0de34b18f0aa69c
Maximiliano Sena <130717933+maximilianosena@users.noreply.github.com>
2024-12-09 17:51:53 -0300

    update

--- js/mail.js
+++ js/mail.js
@@ -1,3 +1,8 @@
+function isMobile() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    return /iphone|ipod|android|windows phone|blackberry|iemobile|opera mini/.test(userAgent);
+}
+
 document.getElementById('messageForm').addEventListener('submit', async (e) => {
     e.preventDefault(); 
 
@@ -11,6 +16,30 @@ document.getElementById('messageForm').addEventListener('submit', async (e) => {
         
         let mailtoLink = '';
 
+
+        if (isMobile()) {
+            if (email.includes('@gmail')) {
+                responseMessage.style.display = 'block';
+                responseMessage.textContent = 'Redireccionando a Gmail...';
+                responseMessage.style.color = 'green';
+                
+                mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=soctech@soctech-uy.com.com&su=Consulta de ${encodeURIComponent(name)}&body=Has recibido un mensaje de ${encodeURIComponent(name)} (${encodeURIComponent(email)}):%0D%0A%0D%0A${encodeURIComponent(message)}`;
+            } else if (email.includes('@outlook')) {
+                responseMessage.style.display = 'block';
+                responseMessage.textContent = 'Redireccionando a Outlook...';
+                responseMessage.style.color = 'green';
+                
+                mailtoLink = `https://outlook.live.com/mail/0/deeplink/compose?to=soctech@soctech-uy.com.com&subject=Consulta de ${encodeURIComponent(name)}&body=Has recibido un mensaje de ${encodeURIComponent(name)} (${encodeURIComponent(email)}):%0D%0A%0D%0A${encodeURIComponent(message)}`;
+            } else {
+                responseMessage.style.display = 'block';
+                responseMessage.textContent = 'Correo desconocido, seleccione su proveedor.';
+                responseMessage.style.color = 'yellow';
+                
+                mailtoLink = `mailto:soctech@soctech-uy.com.com?subject=Consulta de ${encodeURIComponent(name)}&body=Has recibido un mensaje de ${encodeURIComponent(name)} (${encodeURIComponent(email)}):%0D%0A%0D%0A${encodeURIComponent(message)}`;
+            }
+        } else {
+
+            
         if (email.includes('@gmail')) {
             responseMessage.style.display = 'block';
             responseMessage.textContent = 'Redireccionando a su correo.';
@@ -28,6 +57,7 @@ document.getElementById('messageForm').addEventListener('submit', async (e) => {
             responseMessage.textContent = 'Correo desconocido, seleccione su proveedor.';
             responseMessage.style.color = 'yellow';
         }
+    }
 
         
         window.location.href = mailtoLink;
